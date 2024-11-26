126377d261bb6181135ee8034c84d21895a007b1
Jahanzaib <jahanzaibarshadrana3838@gmail.com>
2024-11-26 21:27:06 +0500

    Adding Plot file to the Project

---
+++ DataPlot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+categories = ['A', 'B', 'C', 'D']
+values = [23, 45, 56, 78]
+
+plt.bar(categories, values)
+plt.title('Sample Bar Chart')
+plt.xlabel('Categories')
+plt.ylabel('Values')
+plt.show()
