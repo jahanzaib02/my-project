9fc50b4ec4a01eee62baa113820ac9836a9e108d
Jahanzaib <jahanzaibarshadrana3838@gmail.com>
2024-11-26 21:43:40 +0500

    Updated dataset.py

--- Dataset.py
+++ Dataset.py
@@ -3,4 +3,5 @@ import pandas as pd
 data = pd.read_csv('C:\Users\DELL\OneDrive - Higher Education Commission\Semester 5\Programming for AI\LAB3\Iris.csv')
 print("Dataset:")
 print(data.head())
+print("This is added")
 
