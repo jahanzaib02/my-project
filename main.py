46ab6a39f34c431cc9c97d9cde93ded2a73f0784
Jahanzaib <jahanzaibarshadrana3838@gmail.com>
2024-11-26 21:22:21 +0500

    Project Creation

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = pd.read_csv('C:\Users\DELL\OneDrive - Higher Education Commission\Semester 5\Programming for AI\LAB3\Iris.csv')
+print("Dataset:")
+print(data.head())
+
